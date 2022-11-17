7a3a382d75d4085bd0e098c97a6cac9f6d90fb81
aabdelhamid <amamdouh@ejada.com>
2022-11-17 16:26:56 +0200

    uploading framework

--- src/test/java/project/open_url_through_cookies.java
+++ src/test/java/project/open_url_through_cookies.java
@@ -1,14 +1,30 @@
 package project;
 
+import Stage2.HomePage;
 import org.openqa.selenium.WindowType;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class open_url_through_cookies extends TestBase {
 
+    //fe el Testbase .. e3mlhom @BeforeClass w @AfterClass
+
     Cookies cokObj;
-    @Test(priority = 1)
-    public void loginCookies()
-    {
+
+    HomePage Orange;
+
+    @Test (priority = 1)
+    public void signin_Valid() throws InterruptedException, IOException {
+        Orange = new HomePage(AlyDriver);
+        Orange.Login_Valid();
+        Thread.sleep(2000);
+
+    }
+
+
+    @Test (priority = 2)
+    public void loginCookies() throws InterruptedException {
         cokObj = new Cookies();
         AlyDriver.switchTo().newWindow(WindowType.TAB);
         cokObj.writeCookies();
